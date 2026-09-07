e635c72846e5dedaa4925727845b632ab828387f
Hemantnegi017 <hemantnegi0126@gmail.com>
2026-09-07 19:31:53 +0530

    Time: 84 ms (14.35%), Space: 63 MB (55.73%) - LeetHub

---
+++ 2618-check-if-object-instance-of-class/2618-check-if-object-instance-of-class.js
@@ -0,0 +1,25 @@
+/**
+ * @param {*} obj
+ * @param {*} classFunction
+ * @return {boolean}
+ */
+var checkIfInstanceOf = function(obj, classFunction) {
+    if(obj==null || typeof classFunction  !== "function"){
+        return false;
+    }
+    
+    let prototype  = Object.getPrototypeOf(obj);
+
+    while(prototype!== null){
+        if(prototype === classFunction.prototype){
+            return true;
+        }
+
+        prototype = Object.getPrototypeOf(prototype);
+    }
+    return false;
+};
+
+/**
+ * checkIfInstanceOf(new Date(), Date); // true
+ */
